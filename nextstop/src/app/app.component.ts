5ae166d62dd80a8d615e08c62a84ed999c0de491
Elismasher <rist24082001@gmail.com>
2024-12-23 18:31:14 +0100

    add primeflex

--- nextstop/src/app/app.component.ts
+++ nextstop/src/app/app.component.ts
@@ -1,31 +1,15 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { MessageModule } from 'primeng/message';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'nextstop-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    InputTextModule,
-    ButtonModule,
-    MessageModule,
-    FormsModule,
-  ],
+  imports: [CommonModule, RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  text = '';
-
-  msg = '';
-
-  onClick() {
-    this.msg = 'Welcome ' + this.text;
-  }
+  buttonText1 = 'Test';
 }
